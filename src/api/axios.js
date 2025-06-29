c9462b606b71c7d46334cb7b6e0445342200f826
scarl <56886399+scarl-06@users.noreply.github.com>
2025-06-30 04:57:46 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:7078/api',
+  baseURL: 'https://edusync--api-cgehadbsapd6evh0.japanwest-01.azurewebsites.net/api',
 });
 
 api.interceptors.request.use(config => {
