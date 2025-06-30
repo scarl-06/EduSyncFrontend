0d23422f5d8ec7e17de43c9f08150e6f968cd9d0
scarl-06 <yashkumawat552@gmail.com>
2025-06-30 12:30:19 +0530

    AppInsight

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 // index.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'; // Tailwind last, to override Bootstrap where needed
-
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,3 +15,10 @@ root.render(
     <App />
   </BrowserRouter>
 );
+const appInsights = new ApplicationInsights({
+  config: {
+    instrumentationKey: 'd18bf256-d28c-498a-8959-08dc87120046',
+    enableAutoRouteTracking: true
+  }
+});
+appInsights.loadAppInsights();
